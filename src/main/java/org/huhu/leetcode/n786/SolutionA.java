b1643132c36595e715aa55e0e189c466e93b5d8d
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-23 00:38:35 +0000

    786. k-th smallest prime fraction: multiway merge

--- src/main/java/org/huhu/leetcode/n786/SolutionA.java
+++ src/main/java/org/huhu/leetcode/n786/SolutionA.java
@@ -8,12 +8,13 @@ class SolutionA implements Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         int n = arr.length;
         PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> arr[a[0]] * arr[b[1]] - arr[b[0]] * arr[a[1]]);
-        for (int j = 1; j < n; j++) {
-            heap.offer(new int[] { 0, j });
-        }
+        heap.offer(new int[] { 0, n - 1 });
         for (int i = 1; i < k; i++) {
             int[] cur = heap.poll();
             int x = cur[0], y = cur[1];
+            if (x == 0 && y > 0) {
+                heap.offer(new int[] { 0, y - 1 });
+            }
             if (x + 1 < y) {
                 heap.offer(new int[] { x + 1, y });
             }
