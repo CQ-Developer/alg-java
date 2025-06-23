e2b468d5e56524b4399d99371d4dd1d5e9d05e8a
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-23 00:55:55 +0000

    3116. kth smallest amount with single denomination combination

---
+++ src/main/java/org/huhu/leetcode/n3116/Solution.java
@@ -0,0 +1,7 @@
+package org.huhu.leetcode.n3116;
+
+interface Solution {
+
+    long findKthSmallest(int[] coins, int k);
+
+}
---
+++ src/test/java/org/huhu/leetcode/n3116/SolutionTest.java
@@ -0,0 +1,29 @@
+package org.huhu.leetcode.n3116;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+abstract class SolutionTest {
+
+    Solution solution;
+
+    abstract Solution getSolution();
+
+    @BeforeEach
+    void setup() {
+        solution = getSolution();
+    }
+
+    @Test
+    void test1() {
+        assertEquals(9, solution.findKthSmallest(new int[] { 3, 6, 9 }, 3));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(12, solution.findKthSmallest(new int[] { 5, 2 }, 7));
+    }
+
+}
