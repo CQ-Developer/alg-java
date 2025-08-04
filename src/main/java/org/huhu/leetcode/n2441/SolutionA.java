586219083cc557b2fb0b7aa0bcd2d962c895979e
chen qiang <chen_qiang@hoperun.com>
2025-08-04 17:48:40 +0800

    2441. largest positive integer that exists with its negative hash表 数组代替集合

--- src/main/java/org/huhu/leetcode/n2441/SolutionA.java
+++ src/main/java/org/huhu/leetcode/n2441/SolutionA.java
@@ -1,19 +1,19 @@
 package org.huhu.leetcode.n2441;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 class SolutionA implements Solution {
 
     @Override
     public int findMaxK(int[] nums) {
         int res = -1;
-        Set<Integer> caching = new HashSet<>();
+        int[] caching = new int[2001];
+        Arrays.fill(caching, Integer.MIN_VALUE);
         for (int num : nums) {
-            if (caching.contains(-num)) {
+            if (caching[1000 - num] == -num) {
                 res = Math.max(res, Math.abs(num));
             }
-            caching.add(num);
+            caching[num + 1000] = num;
         }
         return res;
     }
