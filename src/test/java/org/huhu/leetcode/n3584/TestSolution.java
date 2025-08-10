37173fc4ad369b456a148dda0636aa8bf36b5b66
Jack <8551681083@163.com>
2025-08-10 12:15:02 +0000

    3584. Maximum Product of First and Last Elements of a Subsequence

--- src/test/java/org/huhu/leetcode/n3584/TestSolution.java
+++ src/test/java/org/huhu/leetcode/n3584/TestSolution.java
@@ -1,9 +1,9 @@
 package org.huhu.leetcode.n3584;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 abstract class TestSolution {
 
@@ -18,19 +18,19 @@ abstract class TestSolution {
 
     @Test
     void test1() {
-        int[] nums = {-1, -9, 2, 3, -2, -3, 1};
+        int[] nums = { -1, -9, 2, 3, -2, -3, 1 };
         assertThat(solution.maximumProduct(nums, 1)).isEqualTo(81);
     }
 
     @Test
     void test2() {
-        int[] nums = {1, 3, -5, 5, 6, -4};
+        int[] nums = { 1, 3, -5, 5, 6, -4 };
         assertThat(solution.maximumProduct(nums, 3)).isEqualTo(20);
     }
 
     @Test
     void test3() {
-        int[] nums = {2, -1, 2, -6, 5, 2, -5, 7};
+        int[] nums = { 2, -1, 2, -6, 5, 2, -5, 7 };
         assertThat(solution.maximumProduct(nums, 2)).isEqualTo(35);
     }
 
