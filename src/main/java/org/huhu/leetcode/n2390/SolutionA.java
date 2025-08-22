3a39cdf9d7fa333fae74754f2b19f917a1b15b24
Jack <8551681083@163.com>
2025-08-22 15:29:47 +0000

    2390. Removing Stars From a String

--- src/main/java/org/huhu/leetcode/n2390/SolutionA.java
+++ src/main/java/org/huhu/leetcode/n2390/SolutionA.java
@@ -5,19 +5,15 @@ class SolutionA implements Solution {
     @Override
     public String removeStars(String s) {
         var builder = new StringBuilder();
-        for (int cnt = 0, i = s.length() - 1; i >= 0; i--) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '*') {
-                cnt++;
+                builder.deleteCharAt(builder.length() - 1);
             } else {
-                if (cnt > 0) {
-                    cnt--;
-                } else {
-                    builder.append(c);
-                }
+                builder.append(c);
             }
         }
-        return builder.reverse().toString();
+        return builder.toString();
     }
 
 }
