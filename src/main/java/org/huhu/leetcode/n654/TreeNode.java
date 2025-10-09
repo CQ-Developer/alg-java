72d0df80a5c8c298fb2af13da44f7d62d2035611
Jack <18551681083@163.com>
2025-10-09 01:07:28 +0000

    654. Maimum Binary Tree

--- src/main/java/org/huhu/leetcode/n654/TreeNode.java
+++ src/main/java/org/huhu/leetcode/n654/TreeNode.java
@@ -21,8 +21,7 @@ class TreeNode {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
+        int prime = 31, result = 1;
         result = prime * result + val;
         result = prime * result + ((left == null) ? 0 : left.hashCode());
         result = prime * result + ((right == null) ? 0 : right.hashCode());
@@ -31,26 +30,33 @@ class TreeNode {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
-            return false;
-        TreeNode other = (TreeNode) obj;
-        if (val != other.val)
-            return false;
-        if (left == null) {
-            if (other.left != null)
+        }
+        if (obj instanceof TreeNode other) {
+            if (val != other.val) {
                 return false;
-        } else if (!left.equals(other.left))
-            return false;
-        if (right == null) {
-            if (other.right != null)
+            }
+            if (left == null) {
+                if (other.left != null) {
+                    return false;
+                }
+            } else if (!left.equals(other.left)) {
                 return false;
-        } else if (!right.equals(other.right))
-            return false;
-        return true;
+            }
+            if (right == null) {
+                if (other.right != null) {
+                    return false;
+                }
+            } else if (!right.equals(other.right)) {
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 
 }
--- src/test/java/org/huhu/leetcode/n654/TestSolution.java
+++ src/test/java/org/huhu/leetcode/n654/TestSolution.java
@@ -1,14 +1,9 @@
 package org.huhu.leetcode.n654;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 abstract class TestSolution {
 
@@ -21,32 +16,20 @@ abstract class TestSolution {
         solution = getSolution();
     }
 
-    @ParameterizedTest
-    @MethodSource("source")
-    void test(int[] nums, TreeNode result) {
-        assertThat(solution.constructMaximumBinaryTree(nums)).isEqualTo(result);
+    @Test
+    void test_1() {
+        int[] nums = { 3, 2, 1, 6, 0, 5 };
+        var expected = new TreeNode(6,
+                new TreeNode(3, null, new TreeNode(2, null, new TreeNode(1))),
+                new TreeNode(5, new TreeNode(0), null));
+        assertThat(solution.constructMaximumBinaryTree(nums)).isEqualTo(expected);
     }
 
-    static Stream<Arguments> source() {
-        return Stream.of(
-                arguments(
-                        new int[] { 3, 2, 1, 6, 0, 5 },
-                        new TreeNode(6,
-                                new TreeNode(3,
-                                        null,
-                                        new TreeNode(2,
-                                                null,
-                                                new TreeNode(1))),
-                                new TreeNode(5,
-                                        new TreeNode(0),
-                                        null))),
-                arguments(
-                        new int[] { 3, 2, 1 },
-                        new TreeNode(3,
-                                null,
-                                new TreeNode(2,
-                                        null,
-                                        new TreeNode(1)))));
+    @Test
+    void test_2() {
+        int[] nums = { 3, 2, 1 };
+        var expected = new TreeNode(3, null, new TreeNode(2, null, new TreeNode(1)));
+        assertThat(solution.constructMaximumBinaryTree(nums)).isEqualTo(expected);
     }
 
 }
