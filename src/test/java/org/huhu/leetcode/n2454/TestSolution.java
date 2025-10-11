e8048368b46db6086cb4cd30f8d81eb4674df329
Jack <8551681083@163.com>
2025-10-11 01:14:19 +0000

    2454. next greater element IV

--- src/test/java/org/huhu/leetcode/n2454/TestSolution.java
+++ src/test/java/org/huhu/leetcode/n2454/TestSolution.java
@@ -1,7 +1,6 @@
 package org.huhu.leetcode.n2454;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,13 +18,13 @@ abstract class TestSolution {
     @Test
     void testA() {
         int[] nums = { 2, 4, 0, 9, 6 };
-        assertThat(solution.secondGreaterElement(nums)).containsExactly(9, 6, 6, -1, -1);
+        Assertions.assertThat(solution.secondGreaterElement(nums)).containsExactly(9, 6, 6, -1, -1);
     }
 
     @Test
     void testB() {
-        int[] nums = { 3, 3 };
-        assertThat(solution.secondGreaterElement(nums)).containsExactly(-1, -1);
+        int[] nums = { 3, 3, };
+        Assertions.assertThat(solution.secondGreaterElement(nums)).containsExactly(-1, -1);
     }
 
 }
