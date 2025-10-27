69a0dd707348f87d16915263bf1fa561c02c5c4b
Jack <18551681083@163.com>
2025-10-27 06:28:16 +0000

    2832. Maximal Range That Each Element Is Maximum in It

--- src/main/java/org/huhu/leetcode/n2832/SolutionA.java
+++ src/main/java/org/huhu/leetcode/n2832/SolutionA.java
@@ -1,30 +1,32 @@
 package org.huhu.leetcode.n2832;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 class SolutionA implements Solution {
 
     @Override
     public int[] maximumLengthOfRanges(int[] nums) {
         int n = nums.length;
-        Deque<Integer> stack = new ArrayDeque<>();
+
+        int k = 0;
+        int[] stk = new int[n];
+
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                stack.pop();
+            while (k > 0 && nums[i] > nums[stk[k - 1]]) {
+                k--;
             }
-            ans[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
+            ans[i] = k == 0 ? -1 : stk[k - 1];
+            stk[k++] = i;
         }
-        stack.clear();
+
+        k = 0;
         for (int i = n - 1; i > -1; i--) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                stack.pop();
+            while (k > 0 && nums[i] > nums[stk[k - 1]]) {
+                k--;
             }
-            ans[i] = (stack.isEmpty() ? n - ans[i] : stack.peek() - ans[i]) - 1;
-            stack.push(i);
+            ans[i] = (k == 0 ? n - ans[i] : stk[k - 1] - ans[i]) - 1;
+            stk[k++] = i;
         }
+
         return ans;
     }
 
