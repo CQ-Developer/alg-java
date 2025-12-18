1876b0a146b481ef98ec67bced6fb865cdbeaf74
chen_hoperun <chen_qiang@hoperun.com>
2025-12-18 07:21:44 +0000

    1446. consecutive characters

--- src/main/java/org/huhu/leetcode/n1446/SolutionA.java
+++ src/main/java/org/huhu/leetcode/n1446/SolutionA.java
@@ -5,13 +5,11 @@ class SolutionA implements Solution {
     @Override
     public int maxPower(String s) {
         int mx = 0, n = s.length();
-        for (int l = 0; l < n;) {
-            int r = l;
+        for (int r = 0, l = 0; l < n; l = r) {
             while (r < n && s.charAt(r) == s.charAt(l)) {
                 r++;
             }
             mx = Math.max(mx, r - l);
-            l = r;
         }
         return mx;
     }
