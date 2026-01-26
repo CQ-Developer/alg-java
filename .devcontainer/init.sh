00f521c2a407253a713c3b81bf3a703b7d1cedb0
chen_qiang@hoperun.com <chen>
2026-01-26 15:08:38 +0800

    update config

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export http_proxy=http://172.17.0.1:10810
-export https_proxy=http://172.17.0.1:10810
-
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 25.0.1-tem
