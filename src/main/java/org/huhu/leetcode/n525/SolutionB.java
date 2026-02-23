f7999fc038f275dd626530810a60d22ea1a3a492
Jack <chen_qiang@hoperun.com>
2026-02-23 22:47:35 +0800

    525. contiguous array

--- src/main/java/org/huhu/leetcode/n525/SolutionB.java
+++ src/main/java/org/huhu/leetcode/n525/SolutionB.java
@@ -7,18 +7,19 @@ class SolutionB implements Solution {
 
     @Override
     public int findMaxLength(int[] nums) {
-        int n = nums.length, mx = 0, p = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        for (int i = 0; i < n; i++) {
-            p += nums[i] == 1 ? 1 : -1;
-            if (map.containsKey(p)) {
-                mx = Math.max(mx, i - map.get(p));
+        int pre = 0, ans = 0;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        cnt.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i] == 1 ? 1 : -1;
+            Integer j = cnt.get(pre);
+            if (j != null) {
+                ans = Math.max(ans, i - j);
             } else {
-                map.put(p, i);
+                cnt.put(pre, i);
             }
         }
-        return mx;
+        return ans;
     }
 
 }
